c4e7d76fa77542eaefe27f9251ca8bc34c8b4b02
Nicolas Senave <nicolas.senave@gmail.com>
2022-12-01 17:00:41 +0100

    add `symLinksCleaning` method in cleaning class

--- src/main/java/fr/insee/lunatic/conversion/JSONCleaner.java
+++ src/main/java/fr/insee/lunatic/conversion/JSONCleaner.java
@@ -1,15 +1,13 @@
 package fr.insee.lunatic.conversion;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-
+import fr.insee.lunatic.Constants;
+import fr.insee.lunatic.utils.XslTransformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.insee.lunatic.Constants;
-import fr.insee.lunatic.utils.XslTransformation;
+import javax.json.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * remove technical attribute as xsi:type
@@ -26,6 +24,9 @@ public class JSONCleaner {
 	
 	public String clean(String jsonString) throws Exception {
 
+		// New step: apply (Java) symLinks cleaning
+		jsonString = symLinksCleaning(jsonString);
+
 		if ((jsonString == null) || (jsonString.length() == 0))
 			return null;
 		InputStream json = new ByteArrayInputStream(wrapJsonWithXml(jsonString).getBytes("UTF-8"));
@@ -34,9 +35,11 @@ public class JSONCleaner {
 	}
 	
 	public String generate(InputStream isFinalInput) throws Exception {
+		// Unchanged step: use XSLT "cleaning" sheet
 		OutputStream osOutputFile = generateOS(isFinalInput);
 		String res = osOutputFile.toString();
 		osOutputFile.close();
+
 		return res;
 	}
 
@@ -63,4 +66,70 @@ public class JSONCleaner {
 				.replaceAll("<", "&lt;")
 				.replaceAll(">", "&gt;");
 	}
+
+	/**
+	 * Given a Lunatic JSON flat questionnaire, replace the source/target fields by key/values in "symLinks" attribute
+	 * in "PairwiseLinks" components to be compliant with the Lunatic JS library. */
+	public String symLinksCleaning(String stringQuestionnaire) throws IOException {
+
+		// Read the json string into a JsonObject
+		JsonReader jsonReader = Json.createReader(
+				new ByteArrayInputStream(stringQuestionnaire.getBytes(StandardCharsets.UTF_8)));
+		JsonObject jsonQuestionnaire = jsonReader.readObject();
+
+		// // We will copy the entire input json object, except the "symLinks" attribute in PairwiseLinks components
+		JsonObjectBuilder jsonQuestionnaireBuilder = Json.createObjectBuilder();
+
+		jsonQuestionnaire.forEach((key, jsonValue) -> {
+			if (!key.equals("components")) {
+				jsonQuestionnaireBuilder.add(key, jsonValue);
+			} else {
+				JsonArray jsonComponents = (JsonArray) jsonValue;
+				JsonArrayBuilder jsonComponentsBuilder = Json.createArrayBuilder();
+				for (JsonValue jsonValue1 : jsonComponents) {
+					JsonObject jsonComponent = (JsonObject) jsonValue1;
+					if (!jsonComponent.getString("componentType").equals("PairwiseLinks")) {
+						jsonComponentsBuilder.add(jsonValue1);
+					} else {
+						JsonObjectBuilder jsonPairwiseBuilder = Json.createObjectBuilder();
+						((JsonObject) jsonValue1).forEach((key2, jsonValue2) -> {
+							if (!key2.equals("symLinks")) {
+								jsonPairwiseBuilder.add(key2, jsonValue2);
+							} else {
+								JsonArray jsonSymLinks = (JsonArray) jsonValue2;
+								JsonObjectBuilder jsonSymLinksBuilder = Json.createObjectBuilder();
+								JsonObjectBuilder jsonLINKSBuilder = Json.createObjectBuilder();
+								((JsonObject) jsonSymLinks.get(0)).getJsonObject("LINKS")
+										.getJsonArray("LINK").forEach(jsonValue3 -> {
+											JsonObject jsonSourceTarget = (JsonObject) jsonValue3;
+											int sourceValue = jsonSourceTarget.getInt("source");
+											if (jsonSourceTarget.get("target") != null) {
+												int targetValue = jsonSourceTarget.getInt("target");
+												jsonLINKSBuilder.add(String.valueOf(sourceValue), String.valueOf(targetValue));
+											} else {
+												jsonLINKSBuilder.addNull(String.valueOf(sourceValue));
+											}
+										});
+								jsonSymLinksBuilder.add("LINKS", jsonLINKSBuilder.build());
+								jsonPairwiseBuilder.add("symLinks", jsonSymLinksBuilder.build());
+							}
+						});
+						jsonComponentsBuilder.add(jsonPairwiseBuilder.build());
+					}
+				}
+				jsonQuestionnaireBuilder.add("components", jsonComponentsBuilder.build());
+			}
+		});
+
+		OutputStream outputStream = new ByteArrayOutputStream();
+
+		JsonWriter jsonWriter = Json.createWriter(outputStream);
+		jsonWriter.writeObject(jsonQuestionnaireBuilder.build());
+
+		String result = outputStream.toString();
+		outputStream.close();
+
+		return result;
+	}
+
 }
