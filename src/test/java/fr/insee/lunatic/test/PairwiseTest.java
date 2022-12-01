f91399a03d3730055eaed84d5e636ed2ecb443e6
Nicolas Senave <nicolas.senave@gmail.com>
2022-12-01 16:59:50 +0100

    Create PairwiseTest.java

---
+++ src/test/java/fr/insee/lunatic/test/PairwiseTest.java
@@ -0,0 +1,49 @@
+package fr.insee.lunatic.test;
+
+import fr.insee.lunatic.conversion.JSONCleaner;
+import fr.insee.lunatic.conversion.XMLLunaticFlatToJSONLunaticFlatTranslator;
+import fr.insee.lunatic.conversion.XMLLunaticToXMLLunaticFlatTranslator;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+/** Test class to see if PairwiseLinks object doesn't break translators */
+public class PairwiseTest {
+
+    @Order(1)
+    @Test
+    public void xmlHToXmlFlat() {
+        assertDoesNotThrow(() -> {
+            XMLLunaticToXMLLunaticFlatTranslator translator = new XMLLunaticToXMLLunaticFlatTranslator();
+            String result = translator.generate(this.getClass().getClassLoader().getResourceAsStream("pairwise/pairwise-test.xml"));
+            Files.writeString(Path.of("src/test/resources/pairwise/out/pairwise-flat.xml"), result);
+        });
+    }
+
+    @Order(2)
+    @Test
+    public void xmlFlatToJsonFlat() {
+        assertDoesNotThrow(() -> {
+            XMLLunaticFlatToJSONLunaticFlatTranslator translator2 = new XMLLunaticFlatToJSONLunaticFlatTranslator();
+            String result2 = translator2.translate(new File(this.getClass().getClassLoader().getResource("pairwise/out/pairwise-flat.xml").toURI()));
+            Files.writeString(Path.of("src/test/resources/pairwise/out/pairwise-flat.json"), result2);
+        });
+
+    }
+
+    @Order(3)
+    @Test
+    public void jsonFlatCleaning() {
+        assertDoesNotThrow(() -> {
+            JSONCleaner jsonCleaner = new JSONCleaner();
+            String result3 = jsonCleaner.clean(Files.readString(Path.of("src/test/resources/pairwise/out/pairwise-flat.json")));
+            Files.writeString(Path.of("src/test/resources/pairwise/out/pairwise-flat-cleaned.json"), result3);
+        });
+    }
+
+}
