7b6b9f67a22c34e9aaa4676f639fe7d6dc3d9ef9
Nicolas Senave <nicolas.senave@gmail.com>
2022-12-01 17:27:09 +0100

    remove file writing in PairwiseTest

--- src/test/java/fr/insee/lunatic/test/PairwiseTest.java
+++ src/test/java/fr/insee/lunatic/test/PairwiseTest.java
@@ -3,12 +3,7 @@ package fr.insee.lunatic.test;
 import fr.insee.lunatic.conversion.JSONCleaner;
 import fr.insee.lunatic.conversion.XMLLunaticFlatToJSONLunaticFlatTranslator;
 import fr.insee.lunatic.conversion.XMLLunaticToXMLLunaticFlatTranslator;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -26,37 +21,6 @@ public class PairwiseTest {
                             translator.generate(
                                     this.getClass().getClassLoader().getResourceAsStream("pairwise/pairwise-test.xml")
                             )));
-        });
-    }
-
-    @Order(1)
-    @Test
-    public void xmlHToXmlFlat() {
-        assertDoesNotThrow(() -> {
-            XMLLunaticToXMLLunaticFlatTranslator translator = new XMLLunaticToXMLLunaticFlatTranslator();
-            String result = translator.generate(this.getClass().getClassLoader().getResourceAsStream("pairwise/pairwise-test.xml"));
-            Files.writeString(Path.of("src/test/resources/pairwise/out/pairwise-flat.xml"), result);
-        });
-    }
-
-    @Order(2)
-    @Test
-    public void xmlFlatToJsonFlat() {
-        assertDoesNotThrow(() -> {
-            XMLLunaticFlatToJSONLunaticFlatTranslator translator2 = new XMLLunaticFlatToJSONLunaticFlatTranslator();
-            String result2 = translator2.translate(new File(this.getClass().getClassLoader().getResource("pairwise/out/pairwise-flat.xml").toURI()));
-            Files.writeString(Path.of("src/test/resources/pairwise/out/pairwise-flat.json"), result2);
-        });
-
-    }
-
-    @Order(3)
-    @Test
-    public void jsonFlatCleaning() {
-        assertDoesNotThrow(() -> {
-            JSONCleaner jsonCleaner = new JSONCleaner();
-            String result3 = jsonCleaner.clean(Files.readString(Path.of("src/test/resources/pairwise/out/pairwise-flat.json")));
-            Files.writeString(Path.of("src/test/resources/pairwise/out/pairwise-flat-cleaned.json"), result3);
         });
     }
 
