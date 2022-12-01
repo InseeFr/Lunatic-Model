8ba9634419335d9b31de4257b1557edf35ad3048
Nicolas Senave <nicolas.senave@gmail.com>
2022-12-01 17:24:44 +0100

    same test in one go

--- src/test/java/fr/insee/lunatic/test/PairwiseTest.java
+++ src/test/java/fr/insee/lunatic/test/PairwiseTest.java
@@ -15,6 +15,20 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 /** Test class to see if PairwiseLinks object doesn't break translators */
 public class PairwiseTest {
 
+    @Test
+    public void transformAndClean_questionnaireWithPairwise_doesNotThrow() {
+        XMLLunaticToXMLLunaticFlatTranslator translator = new XMLLunaticToXMLLunaticFlatTranslator();
+        XMLLunaticFlatToJSONLunaticFlatTranslator translator2 = new XMLLunaticFlatToJSONLunaticFlatTranslator();
+        JSONCleaner jsonCleaner = new JSONCleaner();
+        assertDoesNotThrow(() -> {
+            jsonCleaner.clean(
+                    translator2.translate(
+                            translator.generate(
+                                    this.getClass().getClassLoader().getResourceAsStream("pairwise/pairwise-test.xml")
+                            )));
+        });
+    }
+
     @Order(1)
     @Test
     public void xmlHToXmlFlat() {
