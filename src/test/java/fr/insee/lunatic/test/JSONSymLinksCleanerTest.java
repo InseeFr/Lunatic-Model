2a70a44fc905965b5a0804146bd069ea25ae7970
Nicolas Senave <nicolas.senave@gmail.com>
2022-12-02 12:38:18 +0100

    sandbox test on JsonObject getString method

--- src/test/java/fr/insee/lunatic/test/JSONSymLinksCleanerTest.java
+++ src/test/java/fr/insee/lunatic/test/JSONSymLinksCleanerTest.java
@@ -4,9 +4,13 @@ import fr.insee.lunatic.conversion.JSONSymLinksCleaner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JSONSymLinksCleanerTest {
 
@@ -21,5 +25,13 @@ public class JSONSymLinksCleanerTest {
     public void nullString() throws IOException {
         assertNull(symLinksCleaner.clean(null));
     }
-    
+
+    @Test
+    void sandbox_testJsonObjectGetString() {
+        JsonReader reader = Json.createReader(new ByteArrayInputStream("{\"foo\":1}".getBytes()));
+        JsonObject jsonObject = reader.readObject();
+        assertThrows(ClassCastException.class, () -> jsonObject.getString("foo"));
+        assertEquals("1", String.valueOf(jsonObject.get("foo")));
+    }
+
 }
