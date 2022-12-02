80ce536a13024452742705cc2696d8b923761cba
Nicolas Senave <nicolas.senave@gmail.com>
2022-12-02 12:07:17 +0100

    create test class for SymLinks cleaning

---
+++ src/test/java/fr/insee/lunatic/test/JSONSymLinksCleanerTest.java
@@ -0,0 +1,20 @@
+package fr.insee.lunatic.test;
+
+import fr.insee.lunatic.conversion.JSONSymLinksCleaner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class JSONSymLinksCleanerTest {
+
+    JSONSymLinksCleaner symLinksCleaner;
+
+    @BeforeEach
+    public void initCleaner() {
+        symLinksCleaner = new JSONSymLinksCleaner();
+    }
+
+}
