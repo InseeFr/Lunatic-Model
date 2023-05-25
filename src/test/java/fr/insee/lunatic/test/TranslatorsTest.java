281fd725b14a5c1f24cba09143c8abaae46dc08e
Nicolas Senave <nicolas.senave@gmail.com>
2023-05-25 16:33:06 +0200

    style: linter thing

--- src/test/java/fr/insee/lunatic/test/TranslatorsTest.java
+++ src/test/java/fr/insee/lunatic/test/TranslatorsTest.java
@@ -28,32 +28,31 @@ import java.nio.file.Paths;
 public class TranslatorsTest {
 
 	private XMLDiff xmlDiff = new XMLDiff();
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(TranslatorsTest.class);
-	
+
 	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
 	}
-	
-	
+
 	@org.junit.jupiter.api.Test
 	public void testQuestionnaireXMLFToJSONF() {
 		logger.debug("Launch test : XMLLunaticFlat -> JSONLunaticFlat");
 		try {
 			String basePath = Constants.RESOURCES_FOLDER_XMLF_2_JSONF_PATH;
-			
+
 			Path outPath = Paths.get(Constants.TEMP_FOLDER_PATH + "/xmlf-2-jsonf-out.json");
 			Files.deleteIfExists(outPath);
 			Path outputFile = Files.createFile(outPath);
-			
+
 			File in = new File(String.format("%s/form_flat.xml", Constants.RESOURCES_FOLDER_DUMMY_PATH));
-			
+
 			XMLLunaticFlatToJSONLunaticFlatTranslator translator = new XMLLunaticFlatToJSONLunaticFlatTranslator();
 			JSONCleaner jsonCleaner = new JSONCleaner();
-			
+
 			String jsonQuestionnaire = jsonCleaner.clean(translator.translate(in));
 			JSONObject jsonOut = new JSONObject(jsonQuestionnaire);
-			
+
 			Files.write(outputFile, jsonQuestionnaire.getBytes("UTF-8"));
 			logger.debug("File generated at : "+outputFile.toString());
 
@@ -78,30 +77,30 @@ public class TranslatorsTest {
 			Assertions.fail();
 		}
 	}
-	
+
 	@Test
 	public void testQuestionnaireXMLHToXMLF() {
 		logger.debug("Launch test : XMLLunatic -> XMLLunaticFlat");
 
 		try {
 			String basePath = Constants.RESOURCES_FOLDER_XMLH_2_XMLF_PATH;
-			
+
 			Path outPath = Paths.get(Constants.TEMP_FOLDER_PATH + "/xmlh-2-xmlf-out.xml");
 			Files.deleteIfExists(outPath);
 			Path outputFile = Files.createFile(outPath);
-			
+
 			File in = new File(String.format("%s/form.xml",  Constants.RESOURCES_FOLDER_DUMMY_PATH));
-			
+
 			XMLLunaticToXMLLunaticFlatTranslator translator = new XMLLunaticToXMLLunaticFlatTranslator();
 
 			String xmlQuestionnaire = translator.generate(in);
 			Files.write(outputFile, xmlQuestionnaire.getBytes("UTF-8"));
 			logger.debug("File generated at : "+outputFile.toString());
-			
+
 			File expectedFile = new File(String.format("%s/out.xml", basePath));
 			Diff diff = xmlDiff.getDiff(outputFile.toFile(),expectedFile);
 			Assertions.assertFalse(diff.hasDifferences(), getDiffMessage(diff, basePath));
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			Assertions.fail();
@@ -114,33 +113,33 @@ public class TranslatorsTest {
 			Assertions.fail();
 		}
 	}
-	
+
 	@Test
 	public void testQuestionnaireXMLHToJSONF() {
 		logger.debug("Launch test : XMLLunatic -> JSONLunaticFlat");
 		try {
-			
+
 			long startTime = System.currentTimeMillis();
-			
+
 			String basePath = Constants.RESOURCES_FOLDER_XMLH_2_JSONF_PATH;
-			
+
 			Path outPath = Paths.get(Constants.TEMP_FOLDER_PATH + "/xmlh-2-jsonf-out.json");
 			Files.deleteIfExists(outPath);
 			Path outputFile = Files.createFile(outPath);
 
 			File in = new File(String.format("%s/form.xml",  Constants.RESOURCES_FOLDER_DUMMY_PATH));
-			
+
 			XMLLunaticToXMLLunaticFlatTranslator translator = new XMLLunaticToXMLLunaticFlatTranslator();
 			XMLLunaticFlatToJSONLunaticFlatTranslator translator2 = new XMLLunaticFlatToJSONLunaticFlatTranslator();
-			JSONCleaner jsonCleaner = new JSONCleaner();			
-			
+			JSONCleaner jsonCleaner = new JSONCleaner();
+
 			String jsonQuestionnaire = jsonCleaner.clean(translator2.translate(translator.generate(in)));
 			JSONObject jsonOut = new JSONObject(jsonQuestionnaire);
 
 			Files.write(outputFile, jsonQuestionnaire.getBytes("UTF-8"));
-			
+
 			long elapsedTime = System.currentTimeMillis() - startTime;
-			
+
 			logger.debug("File generated at : "+outputFile.toString());
 			logger.debug("Transformation time for eno-XML to json lunatic fo JS: " + elapsedTime +" ms");
 
@@ -163,22 +162,22 @@ public class TranslatorsTest {
 			Assertions.fail();
 		}
 	}
-	
+
 	@Test
 	public void testQuestionnaireXMLHToJSONH() {
 		logger.debug("Launch test : XMLLunatic -> JSONLunaticH");
 		try {
 			String basePath = Constants.RESOURCES_FOLDER_XMLH_2_JSONH_PATH;
-			
+
 			Path outPath = Paths.get(Constants.TEMP_FOLDER_PATH + "/xmlh-2-jsonh-out.json");
 			Files.deleteIfExists(outPath);
 			Path outputFile = Files.createFile(outPath);
-			
+
 			File in = new File(String.format("%s/form.xml",  Constants.RESOURCES_FOLDER_DUMMY_PATH));
 
 			XMLLunaticToJSONLunaticTranslator translator = new XMLLunaticToJSONLunaticTranslator();
 			JSONCleaner jsonCleaner = new JSONCleaner();
-			
+
 			String jsonQuestionnaire = jsonCleaner.clean(translator.translate(in));
 			JSONObject jsonOut = new JSONObject(jsonQuestionnaire);
 			Files.write(outputFile, jsonQuestionnaire.getBytes("UTF-8"));
@@ -210,6 +209,5 @@ public class TranslatorsTest {
 		return String.format("Transformed output for %s should match expected XML document:\n %s", path,
 				diff.toString());
 	}
-	
 	
 }
