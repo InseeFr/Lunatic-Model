d355cd269422f52f27837e78a79bf60e0b3cb600
Nicolas Senave <nicolas.senave@gmail.com>
2023-07-11 16:40:41 +0200

    test: disable tests on xml questionnaires

--- src/test/java/fr/insee/lunatic/test/ValidatorTest.java
+++ src/test/java/fr/insee/lunatic/test/ValidatorTest.java
@@ -4,6 +4,7 @@ import fr.insee.lunatic.test.utils.XMLDiff;
 import fr.insee.lunatic.utils.Modele;
 import fr.insee.lunatic.utils.SchemaValidator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ public class ValidatorTest {
 
 	
 	@Test
+	@Disabled("Xml format will no longer be used in Eno v3")
 	public void testValidateH() {
 		logger.debug("Launch test : Validate Hierarchical modele");
 		String basePath = "src/test/resources/dummy";
@@ -33,6 +35,7 @@ public class ValidatorTest {
 		}
 	}
 	@Test
+	@Disabled("Xml format will no longer be used in Eno v3")
 	public void testValidateF() {
 		logger.debug("Launch test : Validate flat modele");
 		String basePath = "src/test/resources/dummy";
