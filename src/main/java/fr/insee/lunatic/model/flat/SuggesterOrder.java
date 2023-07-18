05b5b505319f0033a2823f3b9bcc08a1b7bd6dab
David Darras <david.darras@insee.fr>
2023-07-18 12:34:29 +0200

    fix: add setters to suggester order

--- src/main/java/fr/insee/lunatic/model/flat/SuggesterOrder.java
+++ src/main/java/fr/insee/lunatic/model/flat/SuggesterOrder.java
@@ -3,12 +3,14 @@ package fr.insee.lunatic.model.flat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
+import lombok.Setter;
 
 @JsonPropertyOrder({
         "field",
         "type"
 })
 @Getter
+@Setter
 public class SuggesterOrder {
 
     @JsonProperty(required = true)
