3a43f96a21c01fa86e3e2296603f934d35dbe739
Nicolas Senave <nicolas.senave@gmail.com>
2023-09-28 14:08:38 +0200

    docs: some javadoc and comments

--- src/main/java/fr/insee/lunatic/model/flat/VariableTypeArray.java
+++ src/main/java/fr/insee/lunatic/model/flat/VariableTypeArray.java
@@ -10,10 +10,12 @@ import lombok.Setter;
 })
 @Getter
 @Setter
-public class VariableTypeArray
-    extends IVariableType
-{
+public class VariableTypeArray extends IVariableType {
 
+    /** Value field for external variables */
     protected ValueTypeArray value;
+
+    /** Values field for collected variables */
     protected ValuesTypeArray values;
+
 }
