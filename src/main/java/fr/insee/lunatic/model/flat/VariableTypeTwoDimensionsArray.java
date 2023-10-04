5d8dbff96dd38a7342316e7407e5c07f6f359a4e
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2023-10-04 10:10:27 +0200

    feat: two dimensions variable array (#118)
    * feat: bi-dimensional variable objects
    
    * test: serialization of bi-dimensional variables
    
    * chore: new feature version
    
    * fix: don't expose logging binding

---
+++ src/main/java/fr/insee/lunatic/model/flat/VariableTypeTwoDimensionsArray.java
@@ -0,0 +1,21 @@
+package fr.insee.lunatic.model.flat;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonPropertyOrder({
+        "value",
+        "values"
+})
+@Getter
+@Setter
+public class VariableTypeTwoDimensionsArray extends IVariableType {
+
+    /** Value field for external variables */
+    protected ValueTypeTwoDimensionsArray value;
+
+    /** Values field for collected variables */
+    protected ValuesTypeTwoDimensionsArray values;
+
+}
