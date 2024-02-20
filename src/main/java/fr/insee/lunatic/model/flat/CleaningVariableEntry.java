652f3089a52a668016ab4cec44b879d5b15bc480
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-02-20 15:45:51 +0100

    refactor: cleaning modeling (#149)
    * refactor: cleaning modeling
    
    * chore: bump version
    
    * refactor: remove unnecessary getters
    
    * refactor(cleaning): add some utility methods
    
    * test: add simple test for json deserializer

---
+++ src/main/java/fr/insee/lunatic/model/flat/CleaningVariableEntry.java
@@ -0,0 +1,59 @@
+package fr.insee.lunatic.model.flat;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+public class CleaningVariableEntry {
+
+    @Getter
+    private String cleaningVariableName;
+
+    @JsonValue
+    private final Map<String, String> cleanedVariables;
+
+
+    public CleaningVariableEntry(final String cleaningVariableName) {
+        this.cleaningVariableName = cleaningVariableName;
+        this.cleanedVariables = new LinkedHashMap<>();
+    }
+
+    /**
+     * Private constructor only meant to be used by jackson (through reflection) for deserialization.
+     * @param cleanedVariables Key value map mapped by jackson.
+     */
+    @JsonCreator @SuppressWarnings("unused")
+    private CleaningVariableEntry(final Map<String, String> cleanedVariables) {
+        this.cleanedVariables = cleanedVariables;
+    }
+
+    public void addCleanedVariable(CleanedVariableEntry cleanedVariableEntry) {
+        if (cleanedVariables.containsKey(cleanedVariableEntry.variableName()))
+            log.warn("Overwriting cleaned variable entry '{}' in cleaning variable entry '{}'",
+                    cleanedVariableEntry.variableName(), cleaningVariableName);
+        cleanedVariables.put(cleanedVariableEntry.variableName(), cleanedVariableEntry.filterExpression());
+    }
+
+    public CleanedVariableEntry getCleanedVariable(String cleanedVariableName) {
+        return new CleanedVariableEntry(cleanedVariableName, cleanedVariables.get(cleanedVariableName));
+    }
+
+    public Set<String> getCleanedVariableNames() {
+        return cleanedVariables.keySet();
+    }
+
+    public int countCleanedVariables() {
+        return cleanedVariables.size();
+    }
+
+    public CleanedVariableEntry removeCleanedVariable(String cleanedVariableName) {
+        return new CleanedVariableEntry(cleanedVariableName, cleanedVariables.remove(cleanedVariableName));
+    }
+
+}
