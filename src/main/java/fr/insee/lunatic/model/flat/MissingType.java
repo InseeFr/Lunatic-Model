6246c38362ef1f10375f90ac119d86db8c54504e
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-02-21 10:28:41 +0100

    refactor: missing block modeling (#153)

--- src/main/java/fr/insee/lunatic/model/flat/MissingType.java
+++ src/main/java/fr/insee/lunatic/model/flat/MissingType.java
@@ -1,22 +1,48 @@
 package fr.insee.lunatic.model.flat;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-@JsonPropertyOrder({
-        "any"
-})
-@Getter
-@Setter
+@Slf4j
 public class MissingType {
 
-    protected List<Object> any;
+    @JsonValue
+    private final Map<String, MissingEntry> missingEntries;
 
     public MissingType() {
-        this.any = new ArrayList<>();
+         missingEntries = new HashMap<>();
     }
+
+    @JsonCreator
+    private MissingType(final Map<String, MissingEntry> missingEntries) {
+        this.missingEntries = missingEntries;
+    }
+
+    public void addMissingEntry(MissingEntry missingEntry) {
+        if (missingEntries.containsKey(missingEntry.getVariableName()))
+            log.warn("Overwriting missing entry for variable '{}'", missingEntry.getVariableName());
+        missingEntries.put(missingEntry.getVariableName(), missingEntry);
+    }
+
+    public MissingEntry getMissingEntry(String variableName) {
+        return missingEntries.get(variableName);
+    }
+
+    public Set<String> getMissingKeys() {
+        return missingEntries.keySet();
+    }
+
+    public int countMissingEntries() {
+        return missingEntries.size();
+    }
+
+    public MissingEntry removeMissingEntry(String variableName) {
+        return missingEntries.remove(variableName);
+    }
+
 }
