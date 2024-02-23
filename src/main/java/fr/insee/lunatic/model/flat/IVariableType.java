60000c63b6008ebfaed21a09f484c65313046af6
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-02-23 11:43:19 +0100

    refactor(variables): initialize variable values (#154)
    * refactor(values): initialize values by default
    
    * refactor(variables): add a dimension property
    
    * refactor(variables): enable variables deserialization
    
    * refactor(variables): int type for dimension

--- src/main/java/fr/insee/lunatic/model/flat/IVariableType.java
+++ src/main/java/fr/insee/lunatic/model/flat/IVariableType.java
@@ -1,10 +1,34 @@
 package fr.insee.lunatic.model.flat;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@JsonPropertyOrder({
+        "name",
+        "variableType",
+        "componentRef",
+        "variableDimension",
+        "value",
+        "values",
+        "expression",
+        "bindingDependencies",
+        "shapeFrom",
+        "inFilter"
+})
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "variableDimension",
+        visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = VariableType.class, name = "0"),
+        @JsonSubTypes.Type(value = VariableTypeArray.class, name = "1"),
+        @JsonSubTypes.Type(value = VariableTypeTwoDimensionsArray.class, name = "2")
+})
 @Getter
 @Setter
 public abstract class IVariableType {
@@ -13,7 +37,19 @@ public abstract class IVariableType {
 
     @JsonProperty(required = true)
     protected String name;
+
+    /**
+     * Response name associated with the variable.
+     * @deprecated Not used by Lunatic anymore.
+     */
+    @Deprecated(since = "3.3.5")
     protected String componentRef;
+
+    @JsonProperty(required = true)
     protected VariableTypeEnum variableType;
+
+    /** Property required to deserialize variable objects. */
+    @JsonProperty(required = true)
+    protected VariableDimension variableDimension;
 
 }
