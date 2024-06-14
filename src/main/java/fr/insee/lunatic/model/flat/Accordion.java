3f08cf2a5c4a08191efc0eed057df69d85fa17b1
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-06-14 11:06:18 +0200

    feat: accordion component (#246)

---
+++ src/main/java/fr/insee/lunatic/model/flat/Accordion.java
@@ -0,0 +1,28 @@
+package fr.insee.lunatic.model.flat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Accordion extends ComponentType {
+
+    List<Item> items;
+
+    public Accordion() {
+        super();
+        this.componentType = ComponentTypeEnum.ACCORDION;
+        this.items = new ArrayList<>();
+    }
+
+    @Getter
+    @Setter
+    public static class Item {
+        LabelType label;
+        LabelType body;
+    }
+
+}
