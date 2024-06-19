9d063d8d02838f1d5df9c397432ee75014dd4124
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-06-19 16:33:50 +0200

    fix: add 'type' property for controls (#250)

--- src/main/java/fr/insee/lunatic/model/flat/ControlCriticalityEnum.java
+++ src/main/java/fr/insee/lunatic/model/flat/ControlCriticalityEnum.java
@@ -1,9 +1,18 @@
 package fr.insee.lunatic.model.flat;
 
+/**
+ * Enum to qualify the criticality level of a control.
+ * Determines how the control is displayed, and if it is blocking or not.
+ */
 public enum ControlCriticalityEnum {
 
+    /** Informative control. */
     INFO,
+
+    /** Warning control. */
     WARN,
+
+    /** Error control, blocking. */
     ERROR
 
 }
