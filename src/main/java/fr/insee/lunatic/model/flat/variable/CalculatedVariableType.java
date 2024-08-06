f9cbbc1b190d233d656d0dccbc50359ee6507651
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-08-06 10:20:58 +0200

    feat: shape from list in calculated variables (#257)

--- src/main/java/fr/insee/lunatic/model/flat/variable/CalculatedVariableType.java
+++ src/main/java/fr/insee/lunatic/model/flat/variable/CalculatedVariableType.java
@@ -1,6 +1,8 @@
 package fr.insee.lunatic.model.flat.variable;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import fr.insee.lunatic.model.flat.LabelType;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +13,6 @@ import java.util.List;
 /**
  * A calculated variable contains an expression to be evaluated at runtime.
  */
-@Getter
-@Setter
 public class CalculatedVariableType extends VariableType {
 
     public CalculatedVariableType() {
@@ -20,14 +20,53 @@ public class CalculatedVariableType extends VariableType {
     }
 
     /** Expression of the calculated variable. */
+    @Getter @Setter
     protected LabelType expression;
 
     /** Name of collected and/or external variables that are required to evaluate the expression. */
+    @Getter @Setter
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     protected List<String> bindingDependencies = new ArrayList<>();
 
-    /** Name of the collected variable that define the 'shape' of the variable.
-     * To be replaced with iteration reference and dimension. */
-    protected String shapeFrom;
+    /** Name of the collected variables that determine the 'shape' of the variable. */
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    protected List<String> shapeFrom = new ArrayList<>();
+
+    /**
+     * Returns the list of the 'shape from' variable names.
+     * @return A list of variable names.
+     */
+    @JsonProperty("shapeFrom")
+    public List<String> getShapeFromList() {
+        return shapeFrom;
+    }
+
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    public void setShapeFrom(List<String> variableNames) {
+        this.shapeFrom = variableNames;
+    }
+
+    /**
+     * Get the first variable in the shape from variables list.
+     * @return A variable name.
+     * @deprecated The shape from property is a list now so this method will be removed at some point.
+     */
+    @Deprecated(since = "3.13.0")
+    public String getShapeFrom() {
+        if (shapeFrom.isEmpty())
+            return null;
+        return shapeFrom.getFirst();
+    }
+
+    /**
+     * Set shape from variable.
+     * @param variableName Variable name.
+     * @deprecated The shape from property is now a list of variable names.
+     */
+    @Deprecated(since = "3.13.0")
+    public void setShapeFrom(String variableName) {
+        shapeFrom.clear();
+        shapeFrom.add(variableName);
+    }
 
 }
