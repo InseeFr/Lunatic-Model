91eb89b4e00b4e2fdc00a7769bfdc3a4f693e892
RemiVerriez <remi.verriez@insee.fr>
2025-05-02 17:38:18 +0200

    feat: min max for dates or durations in bodycell (#295)
    Co-authored-by: Nicolas Senave <nicolas.senave@gmail.com>

--- src/test/java/fr/insee/lunatic/conversion/BodyCellSerializationTest.java
+++ src/test/java/fr/insee/lunatic/conversion/BodyCellSerializationTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.lunatic.model.flat.*;
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -38,6 +39,38 @@ class BodyCellSerializationTest {
               }
             }""";
 
+    private final String jsonInputNumberCellWithDecimals = """
+            {
+              "id": "foo-id",
+              "componentType": "InputNumber",
+              "min": 0.5,
+              "max": 1,
+              "response": {
+                "name": "FOO"
+              }
+            }""";
+
+    private final String jsonDatepickerCell = """
+            {
+              "id": "foo-id",
+              "componentType": "Datepicker",
+              "min": "24/05/1950",
+              "max": "02/04/2030",
+              "response": {
+                "name": "FOO"
+              }
+            }""";
+
+    private ObjectMapper objectMapper;
+
+    /** Direct usage of jackson's object mapper since serialization/deserialization classes can only
+     * work with a questionnaire object for now. */
+    @BeforeEach
+    void setupMapper() {
+        objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
     @Test
     void serializeSuggesterCell() throws JsonProcessingException, JSONException {
         //
@@ -48,8 +81,6 @@ class BodyCellSerializationTest {
         bodyCell.setResponse(new ResponseType());
         bodyCell.getResponse().setName("FOO");
         //
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         String result = objectMapper.writeValueAsString(bodyCell);
         //
         JSONAssert.assertEquals(jsonSuggesterCell, result, JSONCompareMode.STRICT);
@@ -58,7 +89,7 @@ class BodyCellSerializationTest {
     @Test
     void deserializeSuggesterCell() throws JsonProcessingException {
         //
-        BodyCell bodyCell = new ObjectMapper().readValue(jsonSuggesterCell, BodyCell.class);
+        BodyCell bodyCell = objectMapper.readValue(jsonSuggesterCell, BodyCell.class);
         //
         assertEquals("foo-id", bodyCell.getId());
         assertEquals(ComponentTypeEnum.SUGGESTER, bodyCell.getComponentType());
@@ -80,8 +111,6 @@ class BodyCellSerializationTest {
         bodyCell.setResponse(new ResponseType());
         bodyCell.getResponse().setName("FOO");
         //
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         String result = objectMapper.writeValueAsString(bodyCell);
         //
         JSONAssert.assertEquals(jsonInputNumberCell, result, JSONCompareMode.STRICT);
@@ -90,7 +119,7 @@ class BodyCellSerializationTest {
     @Test
     void deserializeInputNumberCell() throws JsonProcessingException {
         //
-        BodyCell bodyCell = new ObjectMapper().readValue(jsonInputNumberCell, BodyCell.class);
+        BodyCell bodyCell = objectMapper.readValue(jsonInputNumberCell, BodyCell.class);
         //
         assertEquals("foo-id", bodyCell.getId());
         assertEquals(ComponentTypeEnum.INPUT_NUMBER, bodyCell.getComponentType());
@@ -98,6 +127,64 @@ class BodyCellSerializationTest {
         assertEquals(100d, bodyCell.getMax());
         assertEquals("%", bodyCell.getUnitLabel().getValue());
         assertEquals(LabelTypeEnum.VTL_MD, bodyCell.getUnitLabel().getType());
+        assertEquals("FOO", bodyCell.getResponse().getName());
+    }
+
+    @Test
+    void serializeInputNumberCell2() throws JsonProcessingException, JSONException {
+        //
+        BodyCell bodyCell = new BodyCell();
+        bodyCell.setId("foo-id");
+        bodyCell.setComponentType(ComponentTypeEnum.INPUT_NUMBER);
+        bodyCell.setMin(0.5d);
+        bodyCell.setMax(1d);
+        bodyCell.setResponse(new ResponseType());
+        bodyCell.getResponse().setName("FOO");
+        //
+        String result = objectMapper.writeValueAsString(bodyCell);
+        //
+        JSONAssert.assertEquals(jsonInputNumberCellWithDecimals, result, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void deserializeInputNumberCell2() throws JsonProcessingException {
+        //
+        BodyCell bodyCell = objectMapper.readValue(jsonInputNumberCellWithDecimals, BodyCell.class);
+        //
+        assertEquals("foo-id", bodyCell.getId());
+        assertEquals(ComponentTypeEnum.INPUT_NUMBER, bodyCell.getComponentType());
+        assertEquals(0.5d, bodyCell.getMin());
+        assertEquals(1d, bodyCell.getMax());
+        assertEquals("FOO", bodyCell.getResponse().getName());
+    }
+
+    @Test
+    void serializeDatepickerCell() throws JsonProcessingException, JSONException {
+        //
+        BodyCell bodyCell = new BodyCell();
+        bodyCell.setId("foo-id");
+        bodyCell.setComponentType(ComponentTypeEnum.DATEPICKER);
+        bodyCell.setMin("24/05/1950");
+        bodyCell.setMax("02/04/2030");
+        bodyCell.setResponse(new ResponseType());
+        bodyCell.getResponse().setName("FOO");
+        //
+        String result = objectMapper.writeValueAsString(bodyCell);
+        //
+        JSONAssert.assertEquals(jsonDatepickerCell, result, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void deserializeDatepickerCell() throws JsonProcessingException {
+        //
+        BodyCell bodyCell = objectMapper.readValue(jsonDatepickerCell, BodyCell.class);
+        //
+        assertEquals("foo-id", bodyCell.getId());
+        assertEquals(ComponentTypeEnum.DATEPICKER, bodyCell.getComponentType());
+        String minValue = (String) bodyCell.getMin();
+        String maxValue = (String) bodyCell.getMax();
+        assertEquals("24/05/1950", minValue);
+        assertEquals("02/04/2030", maxValue);
         assertEquals("FOO", bodyCell.getResponse().getName());
     }
 
