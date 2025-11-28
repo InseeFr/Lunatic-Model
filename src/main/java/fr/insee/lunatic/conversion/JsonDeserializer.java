2d1f1b59e1f24d357d638ba8a8b67bbb386d60e4
nsenave <nicolas.senave@gmail.com>
2025-11-28 10:32:09 +0100

    feat: resetable external variable

--- src/main/java/fr/insee/lunatic/conversion/JsonDeserializer.java
+++ src/main/java/fr/insee/lunatic/conversion/JsonDeserializer.java
@@ -3,16 +3,23 @@ package fr.insee.lunatic.conversion;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.lunatic.exception.SerializationException;
 import fr.insee.lunatic.model.flat.Questionnaire;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Slf4j
 public class JsonDeserializer {
 
-	private static final Logger logger = LoggerFactory.getLogger(JsonDeserializer.class);
+	@Getter
+	private final ObjectMapper mapper;
+
+	public JsonDeserializer() {
+		mapper = new ObjectMapper();
+		// Note: no extra configuration
+	}
 
 	/**
 	 * Deserializes the file at given relative path to a Lunatic questionnaire object.
@@ -26,32 +33,31 @@ public class JsonDeserializer {
 
 		if ((fileName == null) || (fileName.isEmpty())) return null;
 
-		logger.info("Deserializing questionnaire from file {}", fileName);
+		log.info("Deserializing questionnaire from file {}", fileName);
 
-		ObjectMapper mapper = new ObjectMapper();
 		Questionnaire questionnaire;
 		try {
 			questionnaire = mapper.readValue(new StreamSource(fileName).getInputStream(), Questionnaire.class);
 		} catch (IOException e) {
 			throw new SerializationException(e.getMessage(), e);
 		}
-		logger.info("Questionnaire {} successfully deserialized", questionnaire.getId());
+		log.info("Questionnaire {} successfully deserialized", questionnaire.getId());
 		return questionnaire;
 	}
 
 	public Questionnaire deserialize(InputStream jsonQuestionnaire) throws SerializationException {
 		if (jsonQuestionnaire == null) return null;
 
-		logger.debug("Deserializing questionnaire from input stream");
+		log.debug("Deserializing questionnaire from input stream");
 
-		ObjectMapper mapper = new ObjectMapper();
 		Questionnaire questionnaire;
 		try {
 			questionnaire = mapper.readValue(jsonQuestionnaire, Questionnaire.class);
 		} catch (IOException e) {
 			throw new SerializationException(e.getMessage(), e);
 		}
-		logger.info("Questionnaire {} successfully deserialized", questionnaire.getId());
+		log.info("Questionnaire {} successfully deserialized", questionnaire.getId());
 		return questionnaire;
 	}
+
 }
