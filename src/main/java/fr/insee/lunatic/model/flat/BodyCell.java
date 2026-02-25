cd9cb2ff5943b19fa652c0488e16b32815d9bde4
RemiVerriez <remi.verriez@insee.fr>
2026-02-25 14:59:13 +0100

    feat: add properties in class

--- src/main/java/fr/insee/lunatic/model/flat/BodyCell.java
+++ src/main/java/fr/insee/lunatic/model/flat/BodyCell.java
@@ -56,8 +56,25 @@ public class BodyCell {
     @Getter @Setter
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     protected List<Option> options;
-    /** For suggester cells: Name of the code list used for auto-completion. */
 
+    /**
+     * Name of the variable providing dynamic options for QCU cells inside tables.
+     * Same semantics as optionSource in Radio / CheckboxOne / Dropdown components.
+     * If set, the runtime ignores static "options" and loads options
+     * from the referenced variable.
+     */
+    @Getter @Setter
+    protected String optionSource;
+
+    /**
+     * Optional VTL filter applied to dynamic options.
+     * Same semantics as optionFilter in standard QCU components.
+     * Allows filtering the dynamic options depending on context.
+     */
+    @Getter @Setter
+    protected LabelType optionFilter;
+
+    /** For suggester cells: Name of the code list used for auto-completion. */
     @Getter @Setter
     private String storeName;
 
