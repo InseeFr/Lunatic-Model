4e37ce2639338892ceaf6ed7648516b221630249
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-03-18 11:37:42 +0100

    feat: add shouldCheckAllIterations attribute to cleaningExpression

--- src/main/java/fr/insee/lunatic/model/flat/cleaning/CleaningExpression.java
+++ src/main/java/fr/insee/lunatic/model/flat/cleaning/CleaningExpression.java
@@ -16,18 +16,25 @@ public class CleaningExpression {
     private String expression;
     private String shapeFrom;
     @JsonProperty(value = "isAggregatorUsed")
-    private boolean isAggregatorUsed;
+    private Boolean isAggregatorUsed;
+    @JsonProperty(value = "shouldCheckAllIterations")
+    private Boolean shouldCheckAllIterations ;
+
+    public CleaningExpression(String expression, String shapeFrom, boolean isAggregatorUsed) {
+        this.expression = expression;
+        this.shapeFrom = shapeFrom;
+        this.isAggregatorUsed = isAggregatorUsed;
+    }
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        CleaningExpression that = (CleaningExpression) object;
-        return isAggregatorUsed == that.isAggregatorUsed && Objects.equals(expression, that.expression) && Objects.equals(shapeFrom, that.shapeFrom);
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CleaningExpression that = (CleaningExpression) o;
+        return Objects.equals(expression, that.expression) && Objects.equals(shapeFrom, that.shapeFrom) && Objects.equals(isAggregatorUsed, that.isAggregatorUsed) && Objects.equals(shouldCheckAllIterations, that.shouldCheckAllIterations);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(expression, shapeFrom, isAggregatorUsed);
+        return Objects.hash(expression, shapeFrom, isAggregatorUsed, shouldCheckAllIterations);
     }
 }
