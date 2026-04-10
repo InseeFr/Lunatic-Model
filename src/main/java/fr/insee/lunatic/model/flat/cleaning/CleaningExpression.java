5673da2e25c8336cf1723260d88aed6560c33c20
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2026-04-10 15:03:08 +0200

    chore: rename cleaning attribute (#325)

--- src/main/java/fr/insee/lunatic/model/flat/cleaning/CleaningExpression.java
+++ src/main/java/fr/insee/lunatic/model/flat/cleaning/CleaningExpression.java
@@ -14,20 +14,21 @@ public class CleaningExpression {
     private String expression;
     private String shapeFrom;
     /**
-     * isAggregatorUsed: determine if expression includes an aggregator (like count, sum,...)
-     * Use-full to improve performance during resizing
+     * shouldCheckAllIterations: determine if expression need to be evaluated during resizing operations
+     *  Use-full to improve performance during resizing
+     *  Used for expression includes an aggregator (like count, sum,...), or for other business case
      */
-    @JsonProperty(value = "isAggregatorUsed")
-    private Boolean isAggregatorUsed;
+    @JsonProperty(value = "shouldCheckDuringResizing")
+    private Boolean shouldCheckDuringResizing;
     /**
      * shouldCheckAllIterations: determine if expression need to be evaluated at each iteration
      */
     @JsonProperty(value = "shouldCheckAllIterations")
     private Boolean shouldCheckAllIterations ;
 
-    public CleaningExpression(String expression, String shapeFrom, boolean isAggregatorUsed) {
+    public CleaningExpression(String expression, String shapeFrom, Boolean shouldCheckDuringResizing) {
         this.expression = expression;
         this.shapeFrom = shapeFrom;
-        this.isAggregatorUsed = isAggregatorUsed;
+        this.shouldCheckDuringResizing = shouldCheckDuringResizing;
     }
 }
